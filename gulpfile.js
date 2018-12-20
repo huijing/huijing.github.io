9cf7bed2bb86d874d81c335bbbcd2150aed9e870
Chen Hui Jing <kakyou_tensai@yahoo.com>
2018-12-20 21:45:25 +0800

    Fix gulpfile problems

--- gulpfile.js
+++ gulpfile.js
@@ -13,25 +13,9 @@ var messages = {
 }
 
 /**
- * Build the Jekyll Site in development mode
- */
-gulp.task('jekyll-dev', function(done) {
-  browserSync.notify(messages.jekyllDev)
-  return cp.spawn('bundle', ['exec', 'jekyll', 'build', '--drafts', '--future', '--incremental', '--config=_config.yml,_config_dev.yml'], {stdio: 'inherit'})
-    .on('close', done)
-})
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', gulp.series('jekyll-dev', function() { 
-  browserSync.reload()
-}));
-
-/**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future Jekyll builds)
  */
-gulp.task('sass', function() {
+function styles() {
   return gulp.src(['_sass/pages.scss', '_sass/posts.scss'])
     .pipe(sass({
       includePaths: ['scss'],
@@ -41,52 +25,9 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('_site/assets/css/'))
     .pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('assets/css'))
-})
+}
 
-/**
- * Compile files from _js into both _site/js (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('scripts', function() {
-  return gulp.src(['_js/lib/*.js', '_js/custom.js'])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('_site/assets/js'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('assets/js'))
-})
-
-/**
- * Wait for jekyll-dev, then launch the Server
- */
-gulp.task('browser-sync', gulp.series(gulp.parallel('sass', 'scripts'), 'jekyll-dev', function() {
-  browserSync.init({
-    server: '_site',
-    port: 4321
-  })
-}));
-
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run Jekyll & reload BrowserSync
- */
-gulp.task('watch', function() {
-  gulp.watch(['_sass/**/*.scss','_sass/*.scss'], gulp.series('sass'))
-  gulp.watch(['_js/**/*.js'], gulp.series('scripts'))
-  gulp.watch(['index.html', '_layouts/*.html', '_includes/*.html', '_drafts/*', '_posts/*', '_pages/*'], gulp.series('jekyll-rebuild'))
-})
-
-/**
- * Build the Jekyll Site in production mode
- */
-gulp.task('jekyll-prod', function(done) {
-  browserSync.notify(messages.jekyllProd)
-  return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
-    .on('close', done)
-})
-
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('sass-prod', function() {
+function stylesProd() {
   return gulp.src(['_sass/pages.scss', '_sass/posts.scss'])
     .pipe(sass({
       includePaths: ['scss'],
@@ -96,22 +37,87 @@ gulp.task('sass-prod', function() {
     .pipe(cssnano())
     .pipe(gulp.dest('_site/assets/css/'))
     .pipe(gulp.dest('assets/css'))
-})
+}
 
 /**
- * Compile files from _js into both _site/js (for live injecting) and site (for future Jekyll builds)
+ * Compile files from _js into both _site/js (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('scripts-prod', function() {
+function scripts() {
+  return gulp.src(['_js/lib/*.js', '_js/custom.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('_site/assets/js'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('assets/js'))
+}
+
+function scriptsProd() {
   return gulp.src(['_js/lib/*.js', '_js/custom.js'])
     .pipe(concat('scripts.js'))
     .pipe(uglify())
     .pipe(gulp.dest('_site/assets/js'))
     .pipe(gulp.dest('assets/js'))
-})
+}
+
+/**
+ * Server functionality handled by BrowserSync
+ */
+function browserSyncServe(done) {
+  browserSync.init({
+    server: '_site',
+    port: 4321
+  })
+  done();
+}
+
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
+
+/**
+ * Build Jekyll site
+ */
+function jekyllDev(done) {
+  browserSync.notify(messages.jekyllDev)
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build', '--drafts', '--future', '--incremental', '--config=_config.yml,_config_dev.yml'], {stdio: 'inherit'})
+  .on('close', done)
+}
+
+function jekyllProd(done) {
+  browserSync.notify(messages.jekyllProd)
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+  .on('close', done)
+}
+
+/**
+ * Watch source files for changes & recompile
+ * Watch html/md files, run Jekyll & reload BrowserSync
+ */
+function watchMarkup() {
+  gulp.watch(['index.html', '_layouts/*.html', '_includes/*.html', '_drafts/*', '_posts/*', '_pages/*'], gulp.series(jekyllDev, browserSyncReload));
+}
+
+function watchScripts() {
+  gulp.watch(['_js/**/*.js'], scripts)
+}
+
+function watchStyles() { 
+  gulp.watch(['_sass/**/*.scss','_sass/*.scss'], styles)
+}
+
+function watch() {
+  gulp.parallel(watchMarkup, watchScripts, watchStyles)
+}
+
+var compile = gulp.parallel(styles, scripts)
+var compileProd = gulp.parallel(stylesProd, scriptsProd)
+
+var serve = gulp.series(compile, jekyllDev, browserSyncServe)
+var watch = gulp.parallel(watchMarkup, watchScripts, watchStyles)
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the Jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', gulp.series('browser-sync', 'watch'))
-gulp.task('build', gulp.series('scripts-prod', 'sass-prod', 'jekyll-prod'))
+gulp.task('default', gulp.parallel(serve, watch))
+gulp.task('build', gulp.series(compileProd, jekyllProd))
