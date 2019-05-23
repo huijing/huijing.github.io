a740d169c9ba3560a8f11dc13999fafbc5a4529f
Chen Hui Jing <kakyou_tensai@yahoo.com>
2019-05-23 20:42:53 +0800

    Experimental night mode, might be a shitshow

--- _js/custom.js
+++ _js/custom.js
@@ -8,7 +8,29 @@ var lifeInEmoji = [
   '👟',
   '🎹',
   '🎨'
-];
+]
 if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
-  window.location.hash = lifeInEmoji[ Math.floor( Math.random() * ( lifeInEmoji.length - 1 ) ) ];
+  window.location.hash = lifeInEmoji[ Math.floor( Math.random() * ( lifeInEmoji.length - 1 ) ) ]
 }
+
+var blendCheckbox = document.getElementById('blendToggle')
+var blender = document.getElementById('blender')
+blendCheckbox.addEventListener('click', toggleBlend, false)
+
+function toggleBlend(e) {
+  if (e.target.checked) {
+    localStorage.checked = true
+  } else {
+    localStorage.checked = ''
+    blender.classList.remove('active')
+  }
+}
+
+(function() {
+  blendCheckbox.checked = localStorage.checked
+  if (localStorage.checked) {
+    blender.classList.add('active')
+  } else {
+    blender.classList.remove('active')
+  }
+})()
